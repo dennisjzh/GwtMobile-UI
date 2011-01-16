03183b8bdacf7f37e055ce14e646fd683e30ecf9
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-01-16 15:51:24 -0500

    started the work of a kitchen sink demo.

---
+++ kitchensink/com/gwtmobile/ui/kitchensink/client/KitchenSink.java
@@ -0,0 +1,17 @@
+package com.gwtmobile.ui.kitchensink.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.gwtmobile.ui.client.page.PageHistory;
+
+public class KitchenSink implements EntryPoint {
+
+	MainPage mainPage = new MainPage();
+	
+	@Override
+	public void onModuleLoad() {		
+        RootLayoutPanel.get().add(mainPage);
+        PageHistory.add(mainPage);
+	}
+
+}
---
+++ kitchensink/com/gwtmobile/ui/kitchensink/client/MainPage.java
@@ -0,0 +1,21 @@
+package com.gwtmobile.ui.kitchensink.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.gwtmobile.ui.client.page.Page;
+
+public class MainPage extends Page {
+
+	private static MainPageUiBinder uiBinder = GWT
+			.create(MainPageUiBinder.class);
+
+	interface MainPageUiBinder extends UiBinder<Widget, MainPage> {
+	}
+
+	public MainPage() {
+		setWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
