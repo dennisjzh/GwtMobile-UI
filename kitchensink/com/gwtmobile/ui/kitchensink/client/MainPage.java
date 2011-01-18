f4244346aa241b43982e5fc8aa01a4ea2dcecd0c
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-01-18 00:58:13 -0500

    more work on kitchen sink.

--- kitchensink/com/gwtmobile/ui/kitchensink/client/MainPage.java
+++ kitchensink/com/gwtmobile/ui/kitchensink/client/MainPage.java
@@ -2,12 +2,18 @@ package com.gwtmobile.ui.kitchensink.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
+import com.gwtmobile.ui.client.event.SelectionChangedEvent;
 import com.gwtmobile.ui.client.page.Page;
+import com.gwtmobile.ui.client.widgets.ListPanel;
 
 public class MainPage extends Page {
 
+	@UiField ListPanel list;
+	
 	private static MainPageUiBinder uiBinder = GWT
 			.create(MainPageUiBinder.class);
 
@@ -17,5 +23,10 @@ public class MainPage extends Page {
 	public MainPage() {
 		setWidget(uiBinder.createAndBindUi(this));
 	}
+
+    @UiHandler("list")
+	void onListSelectionChanged(SelectionChangedEvent e) {
+    	Window.alert(e.getSelection() + "");
+    }
 
 }
