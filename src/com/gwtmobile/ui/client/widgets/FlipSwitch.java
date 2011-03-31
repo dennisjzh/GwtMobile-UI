6c9792ae3fc34681e3ca32de19021bea5a0dcc57
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-03-31 00:27:00 -0400

    FlipSwitch widget now supports drag events.

--- src/com/gwtmobile/ui/client/widgets/FlipSwitch.java
+++ src/com/gwtmobile/ui/client/widgets/FlipSwitch.java
@@ -16,27 +16,37 @@
 
 package com.gwtmobile.ui.client.widgets;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.HTML;
 import com.gwtmobile.ui.client.event.DragController;
 import com.gwtmobile.ui.client.event.DragEvent;
 import com.gwtmobile.ui.client.event.DragEventsHandler;
+import com.gwtmobile.ui.client.utils.Utils;
 
-public class FlipSwitch extends HTML implements DragEventsHandler, ClickHandler {
+public class FlipSwitch extends WidgetBase 
+	implements DragEventsHandler, ClickHandler, HasValueChangeHandlers<Boolean> {
 
-	protected boolean _value = false;
+	protected boolean _value = true;
+	protected HTML _html = new HTML();
 	
     public FlipSwitch() {
+    	initWidget(_html);
         setStyleName("FlipSwitch");
         addStyleName("Off");
-        addClickHandler(this);
-  	super.setHTML("<div></div><div></div><div><div><div>ON</div><div></div><div>OFF</div></div></div>");
+        _html.addClickHandler(this);
+        _html.setHTML("<div></div><div></div><div><div><div>ON</div><div></div><div>OFF</div></div></div>");
     }
     
     @Override
-    public void setHTML(String html) {
-    	assert false : "HTML/Text cannot be set to a FlipSwith widget.";
+    protected void onInitialLoad() {
+    	super.onInitialLoad();
+    	updateFlipPosition();
     }
     
     @Override
@@ -52,19 +62,61 @@ public class FlipSwitch extends HTML implements DragEventsHandler, ClickHandler 
     
     @Override
     public void onDragStart(DragEvent e) {
+    	DragController.get().catchDragEvents(this);
+    	Utils.setTransitionDuration(getFilpElement(), 0);
     }
 
     @Override
     public void onDragMove(DragEvent e) {
+    	e.stopPropagation();
+    	Element flip = getFilpElement();
+    	int x = Utils.getTranslateX(flip);
+    	int newX = (int) (x + e.OffsetX);
+    	int onPosition = getOnPosition();
+    	int offPosition = getOffPosition();
+    	if (newX > onPosition) {
+    		newX = onPosition;
+    	}
+    	else if (newX < offPosition) {
+    		newX = offPosition;
+    	}
+    	if (newX != x) {
+        	Utils.setTranslateX(flip, newX);
+    	}
     }
 
     @Override
     public void onDragEnd(DragEvent e) {
+    	DragController.get().releaseCatch(this);
+    	Element flip = getFilpElement();
+    	int x = Utils.getTranslateX(flip);
+    	int onPosition = getOnPosition();
+    	int offPosition = getOffPosition();
+    	if (x == onPosition) {
+    		setValue(true);
+    	}
+    	else if (x == offPosition) {
+    		setValue(false);
+    	}
+    	else {
+	    	boolean newValue = x >= (offPosition - onPosition) / 2;
+	    	setValue(newValue, true);
+    	}
+    }
+
+    public void setValue(boolean value) {
+    	setValue(value, false);
     }
     
-    public void setValue(boolean value) {
-    	_value = value;
-    	updateStyle();    	
+    public void setValue(boolean value, boolean forceUpdateFlipPosition) {
+    	if (_value != value) {
+        	_value = value;
+        	updateFlipPosition();    	
+    		ValueChangeEvent.fire(this, _value);
+    	}
+    	else if (forceUpdateFlipPosition) {
+        	updateFlipPosition();    	
+    	}
     }
     
     public boolean getValue() {
@@ -73,16 +125,39 @@ public class FlipSwitch extends HTML implements DragEventsHandler, ClickHandler 
 
 	@Override
 	public void onClick(ClickEvent event) {
-		_value = !_value;
-		updateStyle();
+		setValue(!_value);
 	}
 	
-	private void updateStyle() {
+	private void updateFlipPosition() {
+    	Utils.setTransitionDuration(getFilpElement(), 200);
+		Element flip = getFilpElement();
 		if (_value) {
-			removeStyleName("Off");
+			Utils.setTranslateX(flip, getOnPosition());
 		}
 		else {
-			addStyleName("Off");
+			Utils.setTranslateX(flip, getOffPosition());
 		}
+	}
+	
+	private Element getFilpElement() {
+    	return (Element) getElement().getChild(2).getChild(0);
+	}
+
+	private int getOnPosition() {
+		return 0;
+	}
+	
+	private int getOffPosition() {
+		Element flip = getFilpElement();
+		Element parent = flip.getParentElement();
+		int flipWidth = flip.getScrollWidth();
+		int parentWidth = parent.getClientWidth();
+		return parentWidth - flipWidth;
+	}
+
+	@Override
+	public HandlerRegistration addValueChangeHandler(
+			ValueChangeHandler<Boolean> handler) {
+		return this.addHandler(handler, ValueChangeEvent.getType());
 	}
 }
