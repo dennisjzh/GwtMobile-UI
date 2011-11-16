7733715225787a68c640ecd5967b5c3a2cfce996
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-11-15 22:34:08 -0500

    added icons for kitchensink.

---
+++ kitchensink/com/gwtmobile/ui/kitchensink/client/icon/IconResources.java
@@ -0,0 +1,26 @@
+package com.gwtmobile.ui.kitchensink.client.icon;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface IconResources extends ClientBundle {
+	
+	@Source("tab0.png")
+	ImageResource tab0();
+
+	@Source("tab0-selected.png")
+	ImageResource tab0selected();
+
+	@Source("tab1.png")
+	ImageResource tab1();
+
+	@Source("tab1-selected.png")
+	ImageResource tab1selected();
+
+	@Source("tab2.png")
+	ImageResource tab2();
+
+	@Source("tab2-selected.png")
+	ImageResource tab2selected();
+
+}
