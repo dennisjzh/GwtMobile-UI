abdb8af224a8c65fb759c2527520d3e28b5fab81
Dennis Z Jiang <jiangzhihua@gmail.com>
2011-11-16 23:51:58 -0500

    Put all the CSS style names used in master.css into one place.

---
+++ src/com/gwtmobile/ui/client/CSS/StyleNames.java
@@ -0,0 +1,50 @@
+package com.gwtmobile.ui.client.CSS;
+
+public interface StyleNames {
+
+	public interface Primary {
+
+		String HVGA = "HVGA";
+		String WVGA = "WVGA";
+		String QVGA = "QVGA";
+
+		String Android = "Android";
+		String iOS = "iOS";
+		String Desktop = "Desktop";
+
+		String Accordion = "Accordion";
+		String AccordionArrow = "AccordionArrow";
+		String BackButton = "BackButton";
+		String Button = "Button";
+		String Chevron = "Chevron";
+		String CheckBoxGroup = "CheckBoxGroup";
+		String CheckBoxIndicator = "CheckBoxIndicator";
+		String DropDownList = "DropDownList";
+		String FlipSwitch = "FlipSwitch";
+		String HeaderPanel = "gwtm-HeaderPanel";
+		String HorizontalPanel = "HorizontalPanel";
+		String ListPanel = "gwtm-ListPanel";
+		String NextButton = "NextButton";
+		String Page = "Page";
+		String Pointer = "Pointer";
+		String RadioButton = "RadioButton";
+		String RadioButtonGroup = "RadioButtonGroup";
+		String SlidePanel = "SlidePanel";
+		String Slide = "Slide";
+		String Slider = "Slider";
+		String ScrollPanel = "gwtm-ScrollPanel";
+		String ScrollBar = "gwtm-ScrollBar";
+		String TabPanel = "TabPanel";
+		String TextArea = "TextArea";
+		String TextBox = "TextBox";
+		String ToolbarPanel = "ToolbarPanel";
+		String VerticalPanel = "VerticalPanel";
+	}
+	
+	public interface Secondary {
+		String Disabled = "Disabled";
+		String Pressed = "Pressed";
+		String WithPadding = "WithPadding";		
+	}
+
+}
