101ffba7ef352ef49f5b69b98c8f048f5adebd3a
baroseiro <baroseiro@gmail.com>
2011-11-24 16:46:53 +0000

    New header simple panel, this is a new version with a simple panel to add anything to the header. The original header stays to give a fixed flavor. Merging both flavors is possible but code-wise would look ugly, so it is better have one class per version.

---
+++ src/com/gwtmobile/ui/client/widgets/HeaderSimplePanel.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2010 Zhihua (Dennis) Jiang
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtmobile.ui.client.widgets;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.gwtmobile.ui.client.CSS.StyleNames.Primary;
+import com.gwtmobile.ui.client.widgets.FlowPanel;
+
+public class HeaderSimplePanel extends FlowPanel implements HasWidgets {
+	
+    public HeaderSimplePanel() {
+        setStyleName(Primary.HeaderPanel);
+    }
+}
