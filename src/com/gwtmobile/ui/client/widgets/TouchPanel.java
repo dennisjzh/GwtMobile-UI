48843d4966dddb16e0af6ec69b8ed200638b2e0d
Frank Mena <frank.mena@noledgy.com>
2012-04-16 18:01:23 -0700

    Added setStopPropagation() and setStartPropagation() to DragControllerMobile.java Added TouchPanel.java that allows automatic propagation to child widgets, such as pan and zoom maps.

---
+++ src/com/gwtmobile/ui/client/widgets/TouchPanel.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2010 Zhihua (Dennis) Jiang
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.gwtmobile.ui.client.widgets;
+
+import com.gwtmobile.ui.client.event.DragController;
+import com.gwtmobile.ui.client.event.DragControllerMobile;
+
+
+
+
+/**
+ * A touch panel that allows propagation of touch events to child widgets.
+ * Use for displaying maps that allow the user to pan and zoom.
+ * 
+ * @author Frank Mena
+ *
+ */
+public class TouchPanel extends PanelBase {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        DragController drag = DragController.get();
+        if (drag instanceof DragControllerMobile)
+            ((DragControllerMobile) drag).setStartPropagation();
+    }
+  
+  @Override
+    public void onUnload() {
+        DragController drag = DragController.get();
+        if (drag instanceof DragControllerMobile)
+            ((DragControllerMobile) drag).setStopPropagation();
+    }
+  
+}
